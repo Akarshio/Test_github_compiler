496e901229e1af27d2a9bb0a7eb3b412dd5e69aa
AKARSH <119515329+Akarshio@users.noreply.github.com>
2025-09-30 11:18:10 +0000

    check 1

--- 1_basic_js/varriable.js
+++ 1_basic_js/varriable.js
@@ -1,7 +1,22 @@
-const StudentId = 1234567;
-let Name = "Akarsh";
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
 
-let Email = "Thisiskarshgupta@gmail.com";
-let Password = "8888"
+// accountId = 2 // not allowed
 
-console.table([ StudentId]);
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
