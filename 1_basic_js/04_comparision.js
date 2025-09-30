71773e9cf5aed628bfa6d55998b0870ac66f9de8
AKARSH <119515329+Akarshio@users.noreply.github.com>
2025-09-30 18:48:48 +0000

    comparision

---
+++ 1_basic_js/04_comparision.js
@@ -0,0 +1,25 @@
+/* console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 != 1);
+console.log(2 == 1); */
+
+//console.log("4" > 1);
+//console.log("09" > 1);
+
+
+/* console.log(null > 0);
+console.log(null >= 0);
+console.log(null < 0);
+console.log(null <= 0);
+console.log(null != 0);
+console.log(null == 0); */
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0)
+
+/// === for strictly check datatype also 
+
+console.log ("2" === 0);
