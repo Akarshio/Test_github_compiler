00fcae621149936372f05f8fbbee6ce4606e9455
AKARSH <119515329+Akarshio@users.noreply.github.com>
2025-09-30 13:47:24 +0000

    datatypes

---
+++ 1_basic_js/datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//  alert(3+3) // not use in nodejs too much, use in browser
+
+console.log(3 + 3) 
+                    // proper wirte to diif output not use signle line print two output 
+console.log("Akarsh")
+
+
+let name = "Akarsh" // string
+let age = 20 // number 
+let isLoggedIn = false // boolean true or false 
+let id; // undefined
+let tem = null // standalone value , if mujhe output mein kuch nhai chaiye then ishka use karenge.
+
+// symbol => unique ke liye use karte hai 
+
+// object  is the datatype of null
+
+console.log (typeof "Akarsh") // 
+console.log(typeof null) // by using null its output is object
+
+console.log(typeof undefined) // output is undefined because undefined is itself datatype
+
+
+
+
