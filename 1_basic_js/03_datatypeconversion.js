3dc26281482575cc488b9de18933262ccbfd566a
AKARSH <119515329+Akarshio@users.noreply.github.com>
2025-09-30 16:24:50 +0000

    conversion of datatypes

---
+++ 1_basic_js/03_datatypeconversion.js
@@ -0,0 +1,39 @@
+let value = true
+
+// after datatype conversion
+
+// undefined => NaN
+// null => 0
+// "30abc" => NaN
+// "Akarsh" => NaN
+//  true => 1
+// false => 0
+
+console.log(typeof value)
+// number conversion 
+let newValue = Number (value)
+
+// boolean conversion
+let islogin = 1
+let booleanIslogin = Boolean (islogin)
+console.log(booleanIslogin);
+
+//  string conversion 
+let num = 10
+let stringNum = String (num)
+
+console.log(typeof stringNum)
+console.log(stringNum)
+
+
+
+
+console.log(typeof newValue)
+console.log(newValue)
+
+
+
+
+
+
+
