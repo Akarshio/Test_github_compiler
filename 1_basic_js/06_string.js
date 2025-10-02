b144e349a4313af6b5764cf11aed294e762fbf3f
AKARSH <119515329+Akarshio@users.noreply.github.com>
2025-10-02 05:42:26 +0000

    strings

---
+++ 1_basic_js/06_string.js
@@ -0,0 +1,15 @@
+const name = "Akarsh"
+let age = 20
+
+console.log(`My name is ${name} & i am ${age} years old.`)
+
+const studentName = new String ("Ram ")
+
+console.log(studentName[0]) // index wise string
+console.log(studentName.trim()); // trim white space
+console.log(studentName.slice(1,3 )) // range output
+console.log(studentName.substring(0,2)) // 
+
+const url = "https://www.akarsh%20gupta.com"
+console.log( url.replace('%20', '_')) // replace
+console.log(url.includes('Akash'))// chech hai ya nhi 
