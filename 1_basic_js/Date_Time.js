a04824c8eeb3d4d6dfff2b3f2f7fc117b3a32bee
AKARSH <119515329+Akarshio@users.noreply.github.com>
2025-12-01 12:50:28 +0000

    Save js

---
+++ 1_basic_js/Date_Time.js
@@ -0,0 +1,27 @@
+let MyDate = new Date()
+// console.log(MyDate);
+
+console.log(MyDate.toDateString());
+console.log(MyDate.toLocaleDateString());
+console.log(MyDate.toLocaleString());
+
+let TimeSpan = Date.now();
+console.log(TimeSpan);
+
+let MyDateCon = new Date("2025-12-01");
+console.log(MyDateCon.getTime());
+
+// if example
+if (true) {
+  console.log("Date object created successfully");
+}
+
+
+
+
+
+
+
+
+
+
